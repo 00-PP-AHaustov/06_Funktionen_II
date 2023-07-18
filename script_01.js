8d1271d8193dd96774c72b2f1e25872c1c2dc924
Anton <anton.haust@gmail.com>
2023-07-18 11:03:55 +0200

    calculator js

--- script_01.js
+++ script_01.js
@@ -1,4 +1,3 @@
-
 "use strict";
 
 /*** Rechner */
@@ -8,17 +7,53 @@
 1. Dateneingabe + -überprüfung : 
 2. Auswahl Rechenart : 
 3. Fkt. Grundrechenarten : 
-4. Ausgabe in Konsole : 
+4. Ausgabe in Konsole : check
 */
+let a, b, c;
 
+input();
+output(calculator(a, b, c));
 
-
-
-
+function input() {
+  do {
+    a = Number(prompt("Erste Zahl:"));
+  } while (isNaN(a));
+  do {
+    b = Number(prompt("Zweite Zahl:"));
+  } while (isNaN(b));
+  do {
+    c = prompt("Bitte wählen: add,sub,mul,div");
+  } while (c != "add" && c != "sub" && c != "mul" && c != "div");
+}
+function addition(num1, num2) {
+  return num1 + num2;
+}
+function substract(num1, num2) {
+  num1 - num2;
+}
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+function divide(num1, num2) {
+  return num1 / num2;
+}
+function calculator(num1, num2, operation) {
+  switch (operation) {
+    case "add":
+      return addition(num1, num2);
+    case "sub":
+      return substract(num1, num2);
+    case "mul":
+      return multiply(num1, num2);
+    case "div":
+      if (num2 == 0) {
+        return "Nicht durch 0 teilbar.";
+      } else return divide(num1, num2);
+  }
+}
 // module: output | test:
 // output("hello");
 // output(2);
 function output(outputData) {
-	console.log(outputData);
+  console.log(outputData);
 }
-  
